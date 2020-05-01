86ae50e3b1d15965ad04357c722f250abd220a26
ipraveenjain <ipraveenjain@gmail.com>
2020-05-01 07:28:19 +0530

    spring rest app initial

---
+++ src/main/java/pbj/springboot/rest/SpringbootresttestApplication.java
@@ -0,0 +1,13 @@
+package pbj.springboot.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootresttestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootresttestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pbj/springboot/rest/SpringbootresttestApplicationTests.java
@@ -0,0 +1,13 @@
+package pbj.springboot.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootresttestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
